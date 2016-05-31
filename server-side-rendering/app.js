69cd94d78da45262925bc5b9e3e2915e5a194288
John Murray <heyjohnmurray@us.ibm.com>
2016-05-31 11:16:27 -0500

    add swig for templating

---
+++ server-side-rendering/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+    swig = require('swig');
+    app = express();
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+app.listen(3000, function(){
+  console.log('The server is listening');
+});
